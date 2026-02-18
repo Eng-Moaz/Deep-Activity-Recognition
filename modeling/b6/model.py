fca20d88e15ac93977c1eda37b4d754441596947
Moaz <moazmohammed198@gmail.com>
2026-02-18 04:35:25 +0200

    hyperparameter edit in b6

--- modeling/b6/model.py
+++ modeling/b6/model.py
@@ -31,11 +31,11 @@ class Baseline6(nn.Module):
 
         # fc layer
         self.fc = nn.Sequential(
-            nn.Linear(cfg.hidden_size, 512),
-            nn.BatchNorm1d(512),
+            nn.Linear(cfg.hidden_size, 128),
+            nn.BatchNorm1d(128),
             nn.ReLU(),
             nn.Dropout(p=cfg.dropout),
-            nn.Linear(512, cfg.num_classes),
+            nn.Linear(128, cfg.num_classes),
         )
 
     def forward(self, x):
