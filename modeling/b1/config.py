065c33c175579b1b6837a7047c0686c6b79f5038
Moaz <moazmohammed198@gmail.com>
2026-02-20 00:13:33 +0200

    configs fixes 2

--- modeling/b1/config.py
+++ modeling/b1/config.py
@@ -37,11 +37,11 @@ class Config:
     ])
 
     # Training
-    batch_size: int = 256
+    batch_size: int = 64
     learning_rate: float = 1e-4
     dropout: float = 0.6
     optimizer: str = "AdamW"
-    weight_decay: float = 0.0001
+    weight_decay: float = 0.01
     epochs: int = 10
     use_amp: bool = True
     patience: int = 5
